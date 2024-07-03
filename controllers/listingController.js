b017d8ab6f377ec43fb007bc1fd1ec4d0db1ccd6
Ruchit <official.ruchit@gmail.com>
2024-07-03 15:01:53 +0530

    add search by name functionality and sory property_type and show count

---
+++ controllers/listingController.js
@@ -0,0 +1,67 @@
+const Listing = require("../models/listingModel");
+
+exports.listingData = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  try {
+    const totalData = await Listing.countDocuments();
+    const totalpage = Math.ceil(totalData / limit);
+    const results = await Listing.aggregate([
+      {
+        $group: {
+          _id: "$property_type",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { _id: 1, count: 1 },
+      },
+      {
+        $limit: limit,
+      },
+    ]);
+
+    res.json({
+      totalData,
+      page,
+      limit,
+      totalpage,
+      results,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch property types" });
+  }
+};
+exports.searchByName = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const name = req.query.name;
+
+  try {
+    const totalData = await Listing.countDocuments({ name: { $regex: name } });
+    const totalpage = Math.ceil(totalData / limit);
+    const results = await Listing.find(
+      {
+        name: {
+          $regex: name,
+        },
+      },
+      {
+        name: 1,
+        _id: 0,
+      }
+    ).limit(limit);
+
+    res.json({
+      totalData,
+      page,
+      totalpage,
+      limit,
+      results,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch listings by name" });
+  }
+};
